908fabcc1b5ab0cfffd546c73ff967d31c176ce8
Gabriel Freitas <ggfto@outlook.com>
2026-05-05 12:11:37 -0300

    feat: add mock data and browser debug system

---
+++ web/src/mock/data.js
@@ -0,0 +1,23 @@
+const mockOptions = {
+  options: {
+    client: [
+      { label: "Interagir com Objeto", icon: "fas fa-hand-paper" },
+      { label: "Ver Informações", icon: "fas fa-info-circle" },
+      { label: "Remover Item", icon: "fas fa-trash" },
+      { label: "Usar Ferramenta", icon: "fas fa-tools" },
+      { label: "Abrir Inventário", icon: "fas fa-box-open" },
+    ],
+    server: [
+      { label: "Comprar Item", icon: "fas fa-shopping-cart" },
+      { label: "Vender Item", icon: "fas fa-coins" },
+    ]
+  },
+  zones: [
+    [
+      { label: "Entrar no Veículo", icon: "fas fa-car" },
+      { label: "Abrir Porta", icon: "fas fa-door-open" },
+    ]
+  ]
+};
+
+export default mockOptions;
